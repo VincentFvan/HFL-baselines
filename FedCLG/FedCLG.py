ae1af51a2bafe6182748c1781c953d27acd55180
VincentFvan <994992433@qq.com>
2024-12-10 17:56:12 +0800

    modify fedclg

--- FedCLG/FedCLG.py
+++ FedCLG/FedCLG.py
@@ -74,7 +74,8 @@ client_idxs = partition_dataset(train_dataset, num_clients, non_iid)
 
 def client_update(client_model, optimizer, train_loader, epoch, device, correction=None):
     client_model.train()
-    for _ in range(epoch):
+    print(f"Client Learning Rate: {optimizer.param_groups[0]['lr']}")
+    for _ in range(epoch):           # client local epoch
         for data, target in train_loader:
             data, target = data.to(device), target.to(device)
             optimizer.zero_grad()
@@ -86,10 +87,12 @@ def client_update(client_model, optimizer, train_loader, epoch, device, correcti
                 for param, corr in zip(client_model.parameters(), correction):
                     param.grad.data += corr.to(device)
             optimizer.step()
+
             
 def server_update(global_model, server_model, server_optimizer, server_loader, epoch, device):
     server_model.load_state_dict(global_model.state_dict())
     server_model.train()
+    print(f"Server Learning Rate: {server_optimizer.param_groups[0]['lr']}")
     for _ in range(epoch):
         for data, target in server_loader:
             data, target = data.to(device), target.to(device)
@@ -110,11 +113,16 @@ def fedclg_c(global_model, clients_models, server_model, server_loader, client_l
 
     # 初始化优化器
     server_optimizer = optim.SGD(server_model.parameters(), lr=server_lr)
+    # 为server设置学习率调度器
+    server_scheduler = optim.lr_scheduler.ExponentialLR(server_optimizer, gamma=0.99)
+    min_lr = 0.001  # 最小学习率
     
     for round_idx in range(num_rounds):
         print(f'Round {round_idx+1}/{num_rounds}')
         # 选择参与的客户端
         selected_clients = np.random.choice(range(num_clients), num_clients_per_round, replace=False)
+        
+        
         # 服务器计算梯度g_s
         server_model.load_state_dict(global_model.state_dict())
         server_model.eval()
@@ -123,17 +131,23 @@ def fedclg_c(global_model, clients_models, server_model, server_loader, client_l
             data, target = data.to(device), target.to(device)
             output = server_model(data)
             loss = nn.CrossEntropyLoss()(output, target)
-            loss.backward()
+            loss.backward()   # 反向传播，计算梯度
             for param in server_model.parameters():
-                g_s.append(param.grad.data.clone())
-            break  # 仅计算一批用于计算g_s，实际可根据论文进行修改
+                g_s.append(param.grad.data.clone())   # 将每个参数的梯度存入g_s
+            break  # 仅加载一batch(64)用于计算g_s，论文中也可以用全部数据。数据越多越准确
 
         # 客户端更新
         for client_idx in selected_clients:
             client_model = clients_models[client_idx]
-            client_model.load_state_dict(global_model.state_dict())
+            client_model.load_state_dict(global_model.state_dict())       # 将全局模型参数分发到clients
             client_model.to(device)
             optimizer = optim.SGD(client_model.parameters(), lr=client_lr)
+            
+            # 为客户端设置学习率调度器
+            scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+            client_optimizers.append(optimizer)
+            client_schedulers.append(scheduler)
+            
             # 计算校正项c_i = g_s - g_i
             client_model.eval()
             g_i = []
@@ -144,7 +158,7 @@ def fedclg_c(global_model, clients_models, server_model, server_loader, client_l
                 loss.backward()
                 for param in client_model.parameters():
                     g_i.append(param.grad.data.clone())
-                break  # 仅计算一批用于计算g_i，实际可根据论文进行修改
+                break  # 仅计算一批（64）用于计算g_i，论文中提出了也可以用全部数据
 
             correction = []
             for gs, gi in zip(g_s, g_i):
@@ -170,7 +184,25 @@ def fedclg_c(global_model, clients_models, server_model, server_loader, client_l
         # 服务器本地训练
         server_update(global_model, server_model, server_optimizer, server_loader, server_epochs, device)
         
+        # 在每一轮结束后，执行学习率衰减
+        for scheduler, optimizer in zip(client_schedulers, client_optimizers):
+            scheduler.step()
+            adjust_learning_rate(optimizer, min_lr)
+        server_scheduler.step()
+        adjust_learning_rate(server_optimizer, min_lr)
+
+
+  
         
+def adjust_learning_rate(optimizer, min_lr):
+    for param_group in optimizer.param_groups:
+        if param_group['lr'] < min_lr:
+            param_group['lr'] = min_lr
+            
+
+
+
+  
 def fedclg_s(global_model, clients_models, server_model, server_loader, client_loaders, num_rounds, num_clients_per_round, 
              client_epochs, server_epochs, client_lr, server_lr, device):
     num_clients = len(clients_models)
@@ -179,11 +211,13 @@ def fedclg_s(global_model, clients_models, server_model, server_loader, client_l
 
     # 初始化优化器
     server_optimizer = optim.SGD(server_model.parameters(), lr=server_lr)
+    # 为server设置学习率调度器
+    server_scheduler = optim.lr_scheduler.ExponentialLR(server_optimizer, gamma=0.99)
+    min_lr = 0.001  # 最小学习率
     
     for round_idx in range(num_rounds):
         print(f'Round {round_idx+1}/{num_rounds}')
-        # 选择参与的客户端
-        selected_clients = np.random.choice(range(num_clients), num_clients_per_round, replace=False)
+
         # 服务器计算梯度g_s
         server_model.load_state_dict(global_model.state_dict())
         server_model.eval()
@@ -196,6 +230,15 @@ def fedclg_s(global_model, clients_models, server_model, server_loader, client_l
             for param in server_model.parameters():
                 g_s.append(param.grad.data.clone())
             break  # 仅计算一批用于计算g_s，实际可根据论文进行修改
+        
+        g_s = [g.cpu() for g in g_s]  # 将梯度移动到 CPU
+        
+        # 选择参与的客户端
+        selected_clients = np.random.choice(range(num_clients), num_clients_per_round, replace=False)
+        
+        # 用于存储客户端的更新 Δ_i 和梯度 g_i
+        delta_list = []
+        g_i_list = []
 
         # 客户端更新
         client_grads = []
@@ -204,57 +247,79 @@ def fedclg_s(global_model, clients_models, server_model, server_loader, client_l
             client_model.load_state_dict(global_model.state_dict())
             client_model.to(device)
             optimizer = optim.SGD(client_model.parameters(), lr=client_lr)
+                        
+            # 为客户端设置学习率调度器
+            scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+            client_optimizers.append(optimizer)
+            client_schedulers.append(scheduler)
             
-            # 客户端训练
-            client_update(client_model, optimizer, client_loaders[client_idx], client_epochs, device)
-            
-            # 计算g_i
+            # ---------- 计算 g_i ----------
             client_model.eval()
             g_i = []
             for data, target in client_loaders[client_idx]:
                 data, target = data.to(device), target.to(device)
+                client_model.zero_grad()
                 output = client_model(data)
                 loss = nn.CrossEntropyLoss()(output, target)
                 loss.backward()
                 for param in client_model.parameters():
                     g_i.append(param.grad.data.clone())
-                break  # 仅计算一批用于计算g_i，实际可根据论文进行修改
-            client_grads.append(g_i)
+                break  # 仅使用一个批次来计算 g_i，可根据需要调整
+            g_i = [g.cpu() for g in g_i]  # 将梯度移动到 CPU
+            g_i_list.append(g_i)
             
-            # 从客户端收集更新
+            # 客户端训练
+            client_update(client_model, optimizer, client_loaders[client_idx], client_epochs, device)
+            
+            # 计算模型更新 Δ_i = x_i^K - x_t
+            delta_i = {}
+            for key in global_model.state_dict().keys():
+                delta_i[key] = (client_model.state_dict()[key] - global_model.state_dict()[key]).detach().cpu()
+            delta_list.append(delta_i)
+            
+            # 将客户端模型移回 CPU
             client_model.to('cpu')
-            client_params = client_model.state_dict()
-            if client_idx == selected_clients[0]:
-                global_params = client_params
-            else:
-                for key in global_params:
-                    global_params[key] += client_params[key]
-        
-        # 计算校正项并更新全局模型
-        for idx, client_idx in enumerate(selected_clients):
-            g_i = client_grads[idx]
+            
+       
+       # ---------- 服务器端聚合更新 ----------
+        # 初始化累计更新
+        aggregated_update = {}
+        for key in global_model.state_dict().keys():
+            aggregated_update[key] = torch.zeros_like(global_model.state_dict()[key])
+            
+        for i in range(len(delta_list)):
+            delta_i = delta_list[i]
+            g_i = g_i_list[i]
+            # 计算校正项 K * eta * (g_s - g_i)
             correction = []
             for gs, gi in zip(g_s, g_i):
-                correction.append(gi - gs)
-            # 更新客户端参数
-            client_model = clients_models[client_idx]
-            client_model.load_state_dict(global_params)
-            for param, corr in zip(client_model.parameters(), correction):
-                param.data -= client_lr * corr / num_clients_per_round
-            # 累计更新后的参数
-            client_params = client_model.state_dict()
-            if idx == 0:
-                updated_params = client_params
-            else:
-                for key in updated_params:
-                    updated_params[key] += client_params[key]
-        for key in updated_params:
-            updated_params[key] = updated_params[key] / num_clients_per_round
-        global_model.load_state_dict(updated_params)
+                correction.append(client_epochs * client_lr * (gs - gi))  # K * η * (g_s - g_i)
+            # 将校正项应用于对应的参数
+            for idx, key in enumerate(global_model.state_dict().keys()):
+                # delta_i[key] 是 Δ_i
+                # correction[idx] 是校正项
+                aggregated_update[key] += (delta_i[key] - correction[idx]) / num_clients_per_round  # 平均化
+             
+        # 更新全局模型参数 x_{t+1}^s = x_t + η_g * aggregated_update
+        global_model.to(device)           #再GPU上进行聚合，这个后面考虑修改
+        with torch.no_grad():
+            for key in global_model.state_dict().keys():
+                global_model.state_dict()[key].add_(global_lr, aggregated_update[key].to(device)) # eta_g是全局学习率
+        global_model.to('cpu')   
+
 
         # 服务器本地训练
         server_update(global_model, server_model, server_optimizer, server_loader, server_epochs, device)
         
+        
+        # 在每一轮结束后，执行学习率衰减
+        for scheduler, optimizer in zip(client_schedulers, client_optimizers):
+            scheduler.step()
+            adjust_learning_rate(optimizer, min_lr)
+        server_scheduler.step()
+        adjust_learning_rate(server_optimizer, min_lr)
+  
+
   
         
 # 初始化模型
@@ -265,25 +330,27 @@ clients_models = [CNNModel() for _ in range(num_clients)]
 # 准备服务器数据加载器（服务器拥有的小数据集）
 server_dataset_size = int(len(train_dataset) * 0.01)  # 服务器数据集占1%
 server_dataset, _ = torch.utils.data.random_split(train_dataset, [server_dataset_size, len(train_dataset) - server_dataset_size])
-server_loader = torch.utils.data.DataLoader(server_dataset, batch_size=32, shuffle=True)
+server_loader = torch.utils.data.DataLoader(server_dataset, batch_size=64, shuffle=True)
 
 # 准备客户端数据加载器
 client_loaders = []
 for idxs in client_idxs:
     client_dataset = torch.utils.data.Subset(train_dataset, idxs)
-    loader = torch.utils.data.DataLoader(client_dataset, batch_size=32, shuffle=True)
+    loader = torch.utils.data.DataLoader(client_dataset, batch_size=64, shuffle=True)
     client_loaders.append(loader)
 
 # 设置设备
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 设置训练参数
-num_rounds = 10
-num_clients_per_round = 10
+num_rounds = 100
+num_clients_per_round = 4   # MNIST参加数目设置为4
 client_epochs = 1
 server_epochs = 1
-client_lr = 0.01
-server_lr = 0.01
+client_lr = 0.05  #MNIST: 从0.01，0.05，0.25三个中调优
+server_lr = 0.05  #MNIST: 从0.01，0.05，0.25三个中调优
+global_lr = 1   # MNIST: 全局学习率为1
+gamma = 0.99      # 衰减系数
 
 # 选择要运行的算法：fedclg_c 或 fedclg_s
 # fedclg_c(global_model, clients_models, server_model, server_loader, client_loaders, num_rounds, num_clients_per_round, 
